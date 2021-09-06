2d72d23f4ceaa6316eb5e3edf02e359117907449
Danil98Borisov <87095649+Danil98Borisov@users.noreply.github.com>
2021-09-06 09:30:39 +0300

    Add files via upload

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,25 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {HotelComponent} from "../hotel/hotel.component";
+import {ApartmentComponent} from "../apartment/apartment.component";
+import { HttpComponent } from 'src/http/http.component';
 
-const routes: Routes = [];
+
+
+const routes: Routes = [
+  {path: 'hotel', component: HotelComponent},
+  {path: 'apartment', component: ApartmentComponent},
+  {path: 'hotel/add', component: HttpComponent}
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports: [
+    RouterModule.forRoot(routes)
+  ],
+  exports: [
+    RouterModule
+  ],
+  declarations: []
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,34 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import {HttpClientModule} from "@angular/common/http";
 import {HotelComponent} from "../hotel/hotel.component";
+import {ApartmentComponent} from "../apartment/apartment.component";
+import { HttpComponent } from 'src/http/http.component';
+
+import {MatTableModule} from '@angular/material/table';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterModule } from '@angular/router';
+import { AppRoutingModule } from './app-routing.module';
+import { FormsModule } from '@angular/forms';
+import { MatTabsModule } from '@angular/material/tabs';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HotelComponent
+    HotelComponent,
+    ApartmentComponent,
+    HttpComponent
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MatTableModule,
+    RouterModule,
+    AppRoutingModule,
+    FormsModule,
+    MatTabsModule
   ],
   providers: [],
-  bootstrap: [AppComponent, HotelComponent]
+  bootstrap: [AppComponent, HotelComponent, ApartmentComponent, HttpComponent]
 })
 export class AppModule { }
