a740b1fb7f80dbc238f620b2c77045bd95b88623
Danil98Borisov <danil1998borisov@gmail.com>
2021-09-07 10:57:16 +0300

    Refactor http.component.ts

--- src/http/http.component.ts
+++ src/http/http.component.ts
@@ -1,22 +1,23 @@
 import { Component} from '@angular/core';
 import { NgForm } from '@angular/forms';
-import { Observable } from 'rxjs/internal/Observable';
 import { Hotel } from 'src/hotel/hotel';
 import { HttpService} from './http.service';
-import {HttpClient, HttpParams} from "@angular/common/http";
+import {HttpClient} from "@angular/common/http";
 
 @Component({
   selector: 'app-http',
-  styleUrls:['http.component.css'],
+  styleUrls: ['http.component.css'],
   templateUrl: 'http.component.html',
   providers: [HttpService]
 })
 export class HttpComponent {
   hotels: Hotel[] = [];
+
   constructor(private http: HttpClient) {
   }
 
   hotelUrlAdd = 'http://localhost:8050/hotel/add';
+
   public addHotel(hotel: Hotel) {
     console.log(JSON.stringify(hotel));
     console.log(this.hotelUrlAdd);
@@ -27,18 +28,11 @@ export class HttpComponent {
       }, error => {
         console.log('error: ', error);
       });
- }
-   onSubmit(form: NgForm){
-      console.log(JSON.stringify(form.value));
-      return this.addHotel(form.value)
-    }
+  }
 
+  onSubmit(form: NgForm) {
+    console.log(JSON.stringify(form.value));
+    return this.addHotel(form.value)
+  }
 
-/*
-  hotelUrlAdd = 'http://localhost:8050/hotel/add';
-  public addHotel(form: NgForm): Observable<Hotel[]> {
-    //return this.http.put<Hotel[]>(this.hotelUrlAdd, {id: hotels.id ,hotelsName: hotels.hotelName,city: hotels.city,rating: hotels.rating});
-    return this.http.put<Hotel[]>(this.hotelUrlAdd, JSON.stringify(form.value));
-    console.log("Отправил на сервер")
-  }*/
 }
