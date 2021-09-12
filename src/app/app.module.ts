bc6fb4bf4cbcd319bcf47d4689a22b39e232d446
Danil98Borisov <danil1998borisov@gmail.com>
2021-09-12 19:02:13 +0300

    Refactor app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,26 +1,26 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
 import {HttpClientModule} from "@angular/common/http";
 import {DeleteHotelComponent} from "../delete/delete-hotel.component";
 import {ApartmentComponent} from "../apartment/apartment.component";
-import { AddHotelComponent } from 'src/add-hotel/add-hotel.component';
-import { HotelComponent } from 'src/hotel/hotel.component';
+import {AddHotelComponent} from 'src/add-hotel/add-hotel.component';
+import {HotelComponent} from 'src/hotel/hotel.component';
 import {EditHotelComponent} from "../edit-hotel/edit-hotel.component";
 import {FilterComponent} from "../filter/filter.component";
 
 import {MatTableModule} from '@angular/material/table';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { RouterModule } from '@angular/router';
-import { AppRoutingModule } from './app-routing.module';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {RouterModule} from '@angular/router';
+import {AppRoutingModule} from './app-routing.module';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
-import { MatTabsModule } from '@angular/material/tabs';
-import { ReservationComponent } from 'src/reservation/reservation.component';
+import {MatTabsModule} from '@angular/material/tabs';
+import {ReservationComponent} from 'src/reservation/reservation.component';
 import {MatDatepickerModule} from "@angular/material/datepicker";
-import {ErrorStateMatcher, MatNativeDateModule, ShowOnDirtyErrorStateMatcher } from '@angular/material/core';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatInputModule } from '@angular/material/input';
+import {ErrorStateMatcher, MatNativeDateModule, ShowOnDirtyErrorStateMatcher} from '@angular/material/core';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
 import {MatSelectModule} from "@angular/material/select";
 import {DatePipe} from "@angular/common";
 import {MatIconModule} from "@angular/material/icon";
@@ -61,4 +61,5 @@ import {MatExpansionModule} from '@angular/material/expansion';
   providers: [DatePipe, {provide: ErrorStateMatcher, useClass: ShowOnDirtyErrorStateMatcher}],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
