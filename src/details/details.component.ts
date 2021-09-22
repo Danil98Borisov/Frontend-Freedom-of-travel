9c2d1ac64ce851eb84b9551ef04fa6ccca37d458
Danil98Borisov <danil1998borisov@gmail.com>
2021-09-22 22:52:54 +0300

    Edit Apartment.

--- src/details/details.component.ts
+++ src/details/details.component.ts
@@ -2,7 +2,10 @@ import {Component, OnInit} from '@angular/core';
 import {ActivatedRoute} from "@angular/router";
 import {DetailsService} from "./details.service";
 import {ApartmentDetails} from "../models/apartmentDetails";
-
+import {AppConstComponent} from "../app/app-const.component";
+import {NgForm} from "@angular/forms";
+import {Apartment} from "../models/apartment";
+import {HttpClient} from "@angular/common/http";
 
 @Component({
   selector: 'app-details',
@@ -17,10 +20,14 @@ export class DetailsComponent implements OnInit {
 
   details: ApartmentDetails = {};
 
+  apartments: Apartment[] = [];
+
   constructor(private activatedRoute: ActivatedRoute,
-              private detailsService: DetailsService
+              private detailsService: DetailsService,
+              private http: HttpClient
   ) {
   }
+  title: string ="qweasadsdqwd"
 
 
   ngOnInit() {
@@ -34,8 +41,29 @@ export class DetailsComponent implements OnInit {
   }
 
   getImageApartment(image: any): any{
-    /*console.log("image = " + image);*/
+    console.log("image = " + image);
     return "data:image/png;base64," + image;
   }
+
+  apartmentUrlEdit = AppConstComponent.API_ENDPOINT + 'apartment/details/edit';
+
+  public editApartment(apartment: Apartment) {
+    if (this.details.apartment) {
+      apartment.id = this.details.apartment.id;
+      apartment.hotel = this.details.apartment.hotel;
+    }
+    return this.http.post<ApartmentDetails>(this.apartmentUrlEdit, {apartment: apartment, apartmentImages: null})
+      .subscribe(editedApartment => {
+        console.log("Апартамент изменён: ", editedApartment);
+        this.details.apartment = editedApartment.apartment;
+      }, error => {
+        console.log('error: ', error);
+      });
+  }
+
+  onSubmitEditApartment(form: NgForm) {
+    return this.editApartment(form.value,)
+  }
+
 
 }
